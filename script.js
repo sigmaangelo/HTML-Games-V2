c743529e2e6c9ec8c1376ec424410dd5ecd8eee3
sigmaangelo <angelo.frank.george@gmail.com>
2025-10-25 18:53:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-
+const games = [
+  {
+  name: "Slope",
+  path: "games/Slope/index.html",
+  icon: "🐤",
+  description:
+    "Flap your wings, dodge pipes, and see how far you can go without crashing! A remake of the classic endless flyer.",
+  category: "Arcade",
+  duration: "Endless",
+  tags: ["arcade", "reflex", "flying", "endless"]
+},const games = [
+  {
+  name: "Slopeeeeeeeee",
+  path: "games/evilglitch/index.html",
+  icon: "🐤",
+  description:
+    "Flap your wings, dodge pipes, and see how far you can go without crashing! A remake of the classic endless flyer.",
+  category: "Arcade",
+  duration: "Endless",
+  tags: ["arcade", "reflex", "flying", "endless"]
+},
